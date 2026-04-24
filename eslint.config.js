9e58b6c2ab5521d94112859157153f84b98af524
Louis Moore <louis.p.moore@gmail.com>
2026-04-23 21:22:37 -0700

    feature: add Stardate capabiliity to Log UI

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import globals from 'globals';
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -24,6 +25,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-hooks/react-compiler': 'off',
+      'react-hooks/set-state-in-effect': 'off',
     },
   },
-])
+]);
